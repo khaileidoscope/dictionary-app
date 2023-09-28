3174b6f12d3171f5845238759831cea23afbce5d
Alvin Shiem Dingcong <alvinshiemd@gmail.com>
2023-09-28 08:59:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // main.js
+const dictionaryApp = document.getElementById("app");
+const inputText = document.getElementById("input-text");
+
+inputText.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+});
